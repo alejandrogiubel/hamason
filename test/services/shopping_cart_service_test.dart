00a5ee8ad0b83c6b121a664287c1232663671626
Alejo <agiubel@gmail.com>
2023-04-28 18:32:58 -0400

    test: add shopping cart service test

--- test/services/shopping_cart_service_test.dart
+++ test/services/shopping_cart_service_test.dart
@@ -1,14 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hamason/app/app.locator.dart';
 import 'package:hamason/models/product.dart';
-import 'package:mockito/mockito.dart';
+import 'package:hamason/services/shopping_cart_service.dart';
 import '../helpers/test_helpers.dart';
-import '../helpers/test_helpers.mocks.dart';
 
 void main() {
   group('ShoppingCartServiceTest -', () {
     setUp(() => registerServices());
-    final shoppingCartService = MockShoppingCartService();
+    final shoppingCartService = ShoppingCartService();
     final product = Product(
       id: 1,
       title: 'Product',
@@ -25,6 +24,19 @@ void main() {
     test('Add product', () {
       shoppingCartService.addProductToShoppingCart(product);
       expect(shoppingCartService.shoppingCart.isEmpty, false);
+    });
+    test('Remove product', () {
+      shoppingCartService.removeProductFromShoppingCart(
+          shoppingCartService.shoppingCart.first);
+      expect(shoppingCartService.shoppingCart.isEmpty, true);
+    });
+    test('Add product again', () {
+      shoppingCartService.addProductToShoppingCart(product);
+      expect(shoppingCartService.shoppingCart.isEmpty, false);
+    });
+    test('Clear shopping cart', () {
+      shoppingCartService.clearShoppingCart();
+      expect(shoppingCartService.shoppingCart.isEmpty, true);
     });
     tearDown(() => locator.reset());
   });
